6919d6ed8acd684c60d75c50ff0c3938556db99c
Bastien Goulet <bastien.goulet27@outlook.com>
2021-03-03 19:50:56 -0500

    Initial commit

---
+++ TP2-Les-arbres-de-decision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP2_Les_arbres_de_decision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
