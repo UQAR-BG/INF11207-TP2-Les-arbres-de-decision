6f544e5bd21fbf9f02e6079874f0e742a8253b9a
Bastien Goulet <bastien.goulet27@outlook.com>
2021-03-04 09:54:37 -0500

    Esait de la lecture d'un fichier Csv dans un DataTable

--- TP2-Les-arbres-de-decision/Program.cs
+++ TP2-Les-arbres-de-decision/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
 
 namespace TP2_Les_arbres_de_decision
 {
@@ -6,7 +10,26 @@ namespace TP2_Les_arbres_de_decision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var reader = new StreamReader("tennis.csv"))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    using (var dr = new CsvDataReader(csv))
+                    {
+                        var dt = new DataTable();
+                        dt.Load(dr);
+
+                        for (int j = 0; j < dt.Rows.Count; j++)
+                        {
+                            for (int i = 0; i < dt.Columns.Count; i++)
+                            {
+                                Console.Write(dt.Columns[i].ColumnName + " ");
+                                Console.WriteLine(dt.Rows[j].ItemArray[i]);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ TP2-Les-arbres-de-decision/Types/Tennis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP2_Les_arbres_de_decision.Types
+{
+    public class Tennis
+    {
+        public string outlook;
+        public string temperature;
+        public string humidity;
+        public string windy;
+        public string play;
+    }
+}
