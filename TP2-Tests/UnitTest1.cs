02fd33b389513846e629c24d0a8800cf1e84097f
Bastien Goulet <bastien.goulet27@outlook.com>
2021-03-03 19:55:57 -0500

    Création du projet de tests et ajout de la librairie CsvHelper

---
+++ TP2-Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TP2_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
